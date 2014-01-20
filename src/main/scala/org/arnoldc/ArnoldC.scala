943c61f342354fced743f8419ce51e1faad82f61
lapa <lapa@lapa.com>
2014-01-20 18:14:33 +0200

    correct filename conversion

--- src/main/scala/org/arnoldc/ArnoldC.scala
+++ src/main/scala/org/arnoldc/ArnoldC.scala
@@ -4,17 +4,24 @@ import java.io.FileOutputStream
 
 object ArnoldC {
   def main(args: Array[String]) {
-    if(args.length != 1){
+    if (args.length != 1) {
       println("Usage: ArnoldC [FileToSourceCode]")
       return
     }
     val filename = args(0)
     val sourceCode = scala.io.Source.fromFile(filename).mkString
-
     val a = new ArnoldGenerator()
-    val bytecode = a.generate(sourceCode, filename)
-    val out = new FileOutputStream(filename+".class")
+    val classFilename = if (filename.contains('.')) {
+      filename.replaceAll("\\.[^.]*$", "")
+    }
+    else {
+      filename
+    }
+    val bytecode = a.generate(sourceCode, classFilename)
+
+    val out = new FileOutputStream(classFilename + ".class")
     out.write(bytecode)
     out.close()
   }
+
 }
